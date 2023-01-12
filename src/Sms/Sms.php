bec19a1a494753a00f57236a842bb750cdd1e663
Sammy M <barsamms@gmail.com>
2023-01-12 13:23:49 +0300

    fix: fixed PSR-4 autoloading standards

--- src/Sms/Sms.php
+++ src/Sms/Sms.php
@@ -7,7 +7,7 @@ class Sms
      * @param string $greet
      * @return string
      */
-    public function great(string $greet = "Hello world"): string
+    public function greet(string $greet = "Hello world"): string
     {
         return $greet;
     }
