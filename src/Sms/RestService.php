4b47e11d78ce5456fdff33552a6722af18599714
masgeek <barsamms@gmail.com>
2026-05-12 09:02:31 +0300

    feat(sms): update constructor to support optional API key

--- src/Sms/RestService.php
+++ src/Sms/RestService.php
@@ -8,14 +8,15 @@ abstract class RestService
 {
     public Client $httpClient;
     public string $baseUrl;
-    protected string $username;
-    protected string $password;
-    protected string $accessToken;
+    protected ?string $username;
+    protected ?string $password;
+    protected ?string $apiKey;
 
-    public function __construct($username, $password)
+    public function __construct(?string $username, ?string $password, ?string $apiKey = null)
     {
         $this->username = $username;
         $this->password = $password;
+        $this->apiKey = $apiKey;
     }
 
     protected function error($data): array
